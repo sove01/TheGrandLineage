7202607b27d750d4a6caeb5e5ef5070907979520
doh46 <do@spsejecna.cz>
2025-02-26 11:57:07 +0100

    test 2

--- src/MapLogic.java
+++ src/MapLogic.java
@@ -1,30 +1,45 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MapLogic {
-    private int RandomSpawn; // start
-    private int currentLocation;
+    private int RandomSpawn = 0; // start
+    private int currentLocation = RandomSpawn;
     private int nextLocation; // from start
 
     public MapLogic() {
 
     }
 
-    // after starting the game, locations and paths will be intialised
-    ArrayList<String> ListOfLocations = new ArrayList<>();
+    public boolean LoadMap(){
+        try (BufferedReader br = new BufferedReader(new FileReader("src\\map.txt"))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                String[] tokens = line.split(";");
 
-    public String map() {
-        try (BufferedReader br = new BufferedReader(new FileReader("src\\Map.csv"))) {
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                ListOfLocations.add(line);
             }
-            return ListOfLocations.toString();
-        } catch (
-                Exception e) {
-
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return "No locations where found";
     }
+    // after starting the game, locations and paths will be intialised
+//    ArrayList<String> ListOfLocations = new ArrayList<>();
+//
+//    public String map() {
+//        try (BufferedReader br = new BufferedReader(new FileReader("src\\Map.txt"))) {
+//            String line = "";
+//            while ((line = br.readLine()) != null) {
+//                ListOfLocations.add(line);
+//            }
+//            return ListOfLocations.toString();
+//        } catch (
+//                Exception e) {
+//
+//        }
+//        return "No locations were found";
+//    }
 }
