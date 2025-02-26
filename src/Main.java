bf45a62f6ebe7b95a1ca2a61ce7538624fe39d8c
doh46 <do@spsejecna.cz>
2025-02-26 11:57:15 +0100

    test 2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        MapLogic m1 = new MapLogic();
+        m1.map();
+    }
+}
